8afa29fa0de090c4a0a83af7be17a899c99751ac
Роман Жуков <zhukovroman82@gmail.com>
2022-09-12 22:18:08 +0300

    initial commit

---
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -0,0 +1,32 @@
+package ru.netology.page;
+
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class VerificationPage {
+    private SelenideElement codeField = $("[data-test-id=code] input");
+    private SelenideElement verifyButton = $("[data-test-id=action-verify]");
+    private SelenideElement error = $("data-test-id=error-notification");
+
+    public void verifyVerificationPageVisibility() {
+        codeField.shouldBe(visible);
+    }
+
+    public void getError() {
+        error.shouldBe(visible);
+    }
+
+    public void verify(String verificationCode) {
+        codeField.setValue(verificationCode);
+        verifyButton.click();
+    }
+
+    public DashboardPage validVerify(String verificationCode) {
+        verify(verificationCode);
+        return new DashboardPage();
+    }
+}
